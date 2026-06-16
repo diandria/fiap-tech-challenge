6c1f328b98fb2d10966932b58bafaf3c8ff9ae96
Diandria Xavier <diandriaxavier@gmail.com>
2026-06-15 21:40:43 -0300

    chore: remove legacy route files from infrastructure/http/routes/

--- src/application/use-cases/service-orders/GetAvgExecutionTimeUseCase.ts
+++
@@ -1,9 +0,0 @@
-import { IServiceOrderRepository, AvgExecutionResult } from '../../../use-cases/ports/IServiceOrderRepository';
-
-export class GetAvgExecutionTimeUseCase {
-  constructor(private readonly osRepo: IServiceOrderRepository) {}
-
-  async execute(): Promise<AvgExecutionResult[]> {
-    return this.osRepo.getAvgExecutionByService();
-  }
-}
