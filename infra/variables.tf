8699e345d731d8ab4367daa82930186f500b39b9
Diandria Xavier <diandriaxavier@gmail.com>
2026-06-16 15:23:44 -0300

    feat: add Terraform module to orchestrate Kubernetes manifests via kubectl provider

---
+++ infra/variables.tf
@@ -0,0 +1,17 @@
+variable "kubeconfig_path" {
+  description = "Caminho para o kubeconfig do Minikube"
+  type        = string
+  default     = "~/.kube/config"
+}
+
+variable "kubeconfig_context" {
+  description = "Contexto do kubeconfig a usar (ex.: minikube)"
+  type        = string
+  default     = "minikube"
+}
+
+variable "namespace" {
+  description = "Namespace Kubernetes onde os recursos serão criados"
+  type        = string
+  default     = "oficina"
+}
