f34fbeed21d992e7472c50b4afd9ffcc142fab48
Diandria Xavier <diandriaxavier@gmail.com>
2026-06-16 21:09:06 -0300

    fix: increase jest timeout and cache mongodb binary in CI

--- jest.config.ts
+++ jest.config.ts
@@ -3,6 +3,7 @@ import type { Config } from 'jest';
 const config: Config = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testTimeout: 30000,
   roots: ['<rootDir>/tests'],
   maxWorkers: 4,
   collectCoverageFrom: [
